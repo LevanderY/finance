57e95c0e3fc2952c470ed5f6c77d77e24e92739d
Yurii <yurii@Andriy-MacBook-Pro.local>
2022-05-23 01:51:41 +0300

    feat: update readme

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
       >
         <Descriptions size="small" column={3}>
           <Descriptions.Item label="Created"><p data-testid='myName'>Yurii Leshyshyn</p></Descriptions.Item>
-          <Descriptions.Item label="Creation Time">23:24</Descriptions.Item>
-          <Descriptions.Item label="Effective Time">01:45</Descriptions.Item>
+          <Descriptions.Item label="Start Time">23:24</Descriptions.Item>
+          <Descriptions.Item label="End Time">01:45</Descriptions.Item>
         </Descriptions>
       </PageHeader>
     )
