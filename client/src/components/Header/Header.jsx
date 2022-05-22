cdef43426ba6616e7cfa43633bc5cb0c69aaf91d
Yurii <yurii@Andriy-MacBook-Pro.local>
2022-05-23 01:45:47 +0300

    feat: for review

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -8,8 +8,8 @@ const Header = () => {
       >
         <Descriptions size="small" column={3}>
           <Descriptions.Item label="Created"><p data-testid='myName'>Yurii Leshyshyn</p></Descriptions.Item>
-          <Descriptions.Item label="Creation Time">2022-05-22</Descriptions.Item>
-          <Descriptions.Item label="Effective Time">.....</Descriptions.Item>
+          <Descriptions.Item label="Creation Time">23:24</Descriptions.Item>
+          <Descriptions.Item label="Effective Time">01:45</Descriptions.Item>
         </Descriptions>
       </PageHeader>
     )
