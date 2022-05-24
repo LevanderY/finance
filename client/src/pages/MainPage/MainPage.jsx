4f4d4d183b97224e653ea626f31859f358431bd5
Yurii <yurii@Andriy-MacBook-Pro.local>
2022-05-25 00:39:39 +0300

    fix: key for table list added

--- client/src/pages/MainPage/MainPage.jsx
+++ client/src/pages/MainPage/MainPage.jsx
@@ -1,11 +1,9 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect, useRef  } from 'react';
-import { Table } from 'antd';
-import socketIOClient from "socket.io-client";
-import { ENDPOINT } from '../../constants'
-import { getTickers } from '../../store/tickers/thunk';
-import TickerDetailButton from '../../components/TickerDetailButton/TickerDetailButton';
-import CheckGrowth from '../../components/CheckGrowth/CheckGrowth';
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect, useRef } from "react";
+import { Table } from "antd";
+import TickerDetailButton from "../../components/TickerDetailButton/TickerDetailButton";
+import CheckGrowth from "../../components/CheckGrowth/CheckGrowth";
+import { getTickersSoket } from "../../helpers/getTickersSoket";
 
 const { Column } = Table;
 
@@ -16,77 +14,58 @@ const MainPage = () => {
     const socketRef = useRef(null);
 
     useEffect(() => {
-        if (socketRef.current == null) {
-          socketRef.current = socketIOClient(ENDPOINT, {
-            cors: {
-              origin: "*",
-            }
-          });
-        }
-    
-        const {current: socket} = socketRef;
-    
-        try {
-          socket.open();
-          socket.emit('start');
-          socket.on('ticker', (data) => {
-            dispatch(getTickers(data));
-          })
-        } catch (error) {
-          console.log(error);
-        }
-        return () => {
-          socket.close();
-        };
-      }, [dispatch]);
+        getTickersSoket(socketRef, dispatch);
+    }, [dispatch]);
 
     return (
         <div>
-            <Table pagination={{position: ['bottomCenter']}} dataSource={tickersList}>
-              <Column 
-                title='Ticker' 
-                dataIndex='ticker' 
-                render={(ticker) => <p data-testid={`tickers-${ticker}`}>{ticker}</p>} 
-              />
-              <Column title='Exchange' dataIndex='exchange' />
-              <Column 
-                title='Price'
-                key={'checkGrowthPrice'}
-                render={({price}) => (
-                    <CheckGrowth growItem={price}/>
-                )}
-              />
-              <Column 
-                title='Change percent'
-                key={'changePercent'}
-                render={({change_percent}) => (
-                    <CheckGrowth growItem={change_percent}/>
-                )}
-              />
-              <Column 
-                title='Dividend'
-                key={'changeDividend'}
-                render={({dividend}) => (
-                    <CheckGrowth growItem={dividend}/>
-                )}
-              />
-              <Column 
-                title='Yield'
-                key={'changeYield'}
-                render={(item) => (
-                    <CheckGrowth growItem={item.yield}/>
-                )}
-              />
-              <Column
-                title={'Show more'}
-                key='showMoreTicker'
-                render={({ticker}) => (
-                 <TickerDetailButton tickerId={ticker}/>
-                )}
-              />
+            <Table
+                pagination={{ position: ["bottomCenter"] }}
+                dataSource={tickersList}
+                rowKey={"ticker"}
+            >
+                <Column
+                    title="Ticker"
+                    dataIndex="ticker"
+                    render={(ticker) => (
+                        <p data-testid={`tickers-${ticker}`}>{ticker}</p>
+                    )}
+                />
+                <Column title="Exchange" dataIndex="exchange" />
+                <Column
+                    title="Price"
+                    key={"checkGrowthPrice"}
+                    render={({ price }) => <CheckGrowth growItem={price} />}
+                />
+                <Column
+                    title="Change percent"
+                    key={"changePercent"}
+                    render={({ change_percent }) => (
+                        <CheckGrowth growItem={change_percent} />
+                    )}
+                />
+                <Column
+                    title="Dividend"
+                    key={"changeDividend"}
+                    render={({ dividend }) => (
+                        <CheckGrowth growItem={dividend} />
+                    )}
+                />
+                <Column
+                    title="Yield"
+                    key={"changeYield"}
+                    render={(item) => <CheckGrowth growItem={item.yield} />}
+                />
+                <Column
+                    title={"Show more"}
+                    key="showMoreTicker"
+                    render={({ ticker }) => (
+                        <TickerDetailButton tickerId={ticker} />
+                    )}
+                />
             </Table>
         </div>
-    )
-}
+    );
+};
 
 export default MainPage;
